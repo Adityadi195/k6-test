48419076cb542103be2f98c0388088ff8fe03b65
Aditya Dwi Irawan <67705557+Adityadi195@users.noreply.github.com>
2024-06-17 01:49:20 +0700

    Update run-load-test.sh

--- run-load-test.sh
+++ run-load-test.sh
@@ -1,5 +1,11 @@
-docker compose up -d influxdb grafana
-echo "--------------------------------------------------------------------------------------"
-echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
-echo "--------------------------------------------------------------------------------------"
-docker compose run --rm k6 run /scripts/ewoks.js
+#!/bin/bash
+
+# Mendeteksi sistem operasi yang digunakan
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Sistem operasi Linux
+    xdg-open "http://localhost:3000/d/k6/hasil-testing"
+# docker compose up -d influxdb grafana
+# echo "--------------------------------------------------------------------------------------"
+# echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
+# echo "--------------------------------------------------------------------------------------"
+# docker compose run --rm k6 run /scripts/ewoks.js
